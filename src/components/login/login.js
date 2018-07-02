1972337af0b5e0b89839de17d43bf04d941342eb
Michael Le-Reiver <m.lereiver@gmail.com>
2018-07-02 12:13:19 -0700

    implemeted signup button

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,51 +4,91 @@ import {Field, reduxForm, focus} from 'redux-form';
 import Input from '../input';
 import {login} from '../../actions/auth-actions';
 import {required, nonEmpty} from '../../validators';
+import RegisterForm from '../register/register';
 
 export class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            signUp: false
+        };
+    }
+
+    goToSignUpPage(e) {
+        e.preventDefault();
+        this.setState({
+            signUp: !(this.state.signUp)
+        });
+    }
+
     onSubmit(values) {
         return this.props.dispatch(login(values.username, values.password));
     }
 
     render() {
-        let error;
+
+        let successMessage;
+
+        if (this.props.submitSucceeded) {
+            successMessage = (
+            <div className="message message-success">
+                You're all logged in!
+            </div>
+            );
+        };
+
+        let errorMessage;
+
         if (this.props.error) {
-            error = (
+            errorMessage = (
                 <div className="form-error" aria-live="polite">
                     {this.props.error}
                 </div>
             );
         }
-        return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                <h2>Login</h2>
-                {error}
-                <label htmlFor="username">Username</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
+
+        if(this.state.signUp) {
+            return <RegisterForm />
+        } else {
+            return (
+                <div>
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    <h3>Login</h3>
+                    {successMessage}
+                    {errorMessage}
+                    <label htmlFor="username">Username</label>
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="username"
+                        id="username"
+                        validate={[required, nonEmpty]}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
+                    </button>
+                </form>
+                <h5>Dont have an account yet?</h5>
+                <button onClick={(e) => this.goToSignUpPage(e)} >
+                    Sign Up
                 </button>
-            </form>
-        );
-    }
+                </div>
+            );
+        }
+  }
 }
 
 export default reduxForm({
